11a1bcd41d82d4ebedb0eb8f4b4ac87bfacb52b2
roomeee <roomasiddiqui2003@gmail.com>
2023-09-06 11:41:51 -1100

    multiple lr from scratch added

--- main.py
+++ main.py
@@ -31,3 +31,15 @@ class MyLR:
         # Make predictions using the trained model
         predictions = [self.m * x + self.b for x in X_test]
         return predictions
+class MyMLR:
+    def __init__(self):
+        self.coef=None
+        self.intercept=None
+    def fit(self, x_train, y_train):
+        x_train=np.insert(x_train,0,1,axis=1)
+        betas=np.linalg.inv(np.dot(x_train.T,x_train)).dot(x_train.T).dot(y_train)
+        self.intercept=betas[0]
+        self.coef=betas[1:]
+
+    def predict(self,X_test):
+        pass
